7c7c0233ed3ff0a28361fb955a2a18721c55de2e
ezgiik <ezgiikarabay@hotmail.com>
2023-10-15 00:38:57 +0300

    Alamofire is added

---
+++ FoodOrderApp/data/entity/CRUDCevap.swift
@@ -0,0 +1,8 @@
+//
+//  CRUDCevap.swift
+//  FoodOrderApp
+//
+//  Created by EZGİ KARABAY on 14.10.2023.
+//
+
+import Foundation
---
+++ FoodOrderApp/data/entity/YemeklerCevap.swift
@@ -0,0 +1,8 @@
+//
+//  YemeklerCevap.swift
+//  FoodOrderApp
+//
+//  Created by EZGİ KARABAY on 14.10.2023.
+//
+
+import Foundation

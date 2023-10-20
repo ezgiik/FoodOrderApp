ecdad498c27520a08772b12b7f7e075ef86db97e
ezgiik <ezgiikarabay@hotmail.com>
2023-10-20 21:23:49 +0300

    navigation title font düzenlendi

--- FoodOrderApp/ui/views/Anasayfa.swift
+++ FoodOrderApp/ui/views/Anasayfa.swift
@@ -30,7 +30,7 @@ class Anasayfa: UIViewController {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = UIColor(named: "anaRenk")
         appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!,
-                                          .font: UIFont(name: "Roboto-Regular", size: 30)!]
+                                          .font: UIFont(name: "Lobster-Regular", size: 30)!]
         
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.compactAppearance = appearance
